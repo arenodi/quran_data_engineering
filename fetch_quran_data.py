1384362fa358a0891a52afa21d62385ef7c697e9
arenodi <andre.arenodi@gmail.com>
2023-09-30 15:48:29 -0300

    feat: added print statements

--- fetch_quran_data.py
+++ fetch_quran_data.py
@@ -25,6 +25,7 @@ for edition_name, identifier in edition_identifiers.items():
         "rawFilepath": raw_filepath,
     }
     raw_editions.append(raw_current)
+    print(f"Raw JSON file generated for edition: {raw_current['name']}.")
 
 for raw_edition in raw_editions:
     transformed_edition = quran_data_handler.quaran_transform(
@@ -33,3 +34,4 @@ for raw_edition in raw_editions:
     quran_data_handler.generate_parsed_file(
         raw_edition["identifier"], raw_edition["name"], transformed_edition
     )
+    print(f"Raw JSON file generated for edition: {raw_edition['identifier']}.")
