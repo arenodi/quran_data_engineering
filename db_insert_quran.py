96d913dd43b90633a59f0dba624de1d0cafe060c
arenodi <andre.arenodi@gmail.com>
2023-09-30 15:08:28 -0300

    chore: refactored the script

--- db_insert_quran.py
+++ db_insert_quran.py
@@ -16,34 +16,24 @@ connection = quran_db_handler.db_connect()
 # get cursor
 cursor = connection.cursor()
 
-setup = quran_db_handler.db_setup("quran_data", cursor)
+db_name = "quran_json"
 
-# if setup is True
-if setup:
-    print("Data base setup successfully")
-    directory = os.getcwd() + "/parsed_data"
-    try:
-        # Edition id
-        edition_id = 1
-        # using the os.listdir function to iterate over the files in directory
-        for json_file in os.listdir(directory):
-            # loads file to python dict
-            with open(f"{directory}/{json_file}") as parsed_quran:
-                # dict to hold the data
-                edition_dict = load(parsed_quran)
+# delete db if exists, holds bool value
+delete_db = quran_db_handler.db_delete_json(db_name, cursor)
 
-            # insert quran data into db
-            insert_data = quran_db_handler.json_to_db(
-                "quran_data", edition_dict, edition_id, cursor
-            )
-            if insert_data:
-                print("Database populated successfully!")
-                connection.commit()
-            # increment edition id by one
-            edition_id += 1
+# if delete successfully
+if delete_db:
+    # setup db successfully, holds bool value
+    setup = quran_db_handler.db_setup_json(db_name, cursor)
 
-    except Exception as e:
-        print(e)
+    # if setup is True
+    if setup:
+        print("Data base setup successfully")
+        directory = os.getcwd() + "/parsed_data"
+        populate = quran_db_handler.populate_db(db_name, cursor, directory)
+        if populate:
+            print("Database populated successfully!")
+
 
 connection.commit()
 
