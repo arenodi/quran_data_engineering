2908898c3fd10984081ba1aa50e009a960b0b19c
arenodi <andre.arenodi@gmail.com>
2023-09-30 15:14:00 -0300

    feat: script to create views for each quran edition in database

---
+++ db_create_views.py
@@ -0,0 +1,24 @@
+# Script to insert data to db
+# Author: arenodi
+
+# Import OS for handling the Json files
+import os
+
+# import load from json lib to  open json file to python obj
+from json import load
+
+# Module for handling the quran db
+import quran_db_handler
+
+
+# connect to database and get the connection obj
+connection = quran_db_handler.db_connect()
+# get cursor
+cursor = connection.cursor()
+
+db_name = "quran_json"
+
+view_create = quran_db_handler.create_views(db_name, cursor)
+
+if view_create:
+    print("Create Views Successfully!")
